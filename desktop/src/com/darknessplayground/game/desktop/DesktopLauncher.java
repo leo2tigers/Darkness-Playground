32850cdfe5a8fcb8707d728211fd6e349af156ab
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2018-11-15 18:12:20 +0700

    Initialization

---
+++ desktop/src/com/darknessplayground/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.darknessplayground.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.darknessplayground.game.DarknessPlayground;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DarknessPlayground(), config);
+	}
+}
