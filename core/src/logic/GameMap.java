e4316ecff42ea4531320d832a1a4bec191fbcc79
Nut Idl <13thfloorguy@gmail.com>
2018-11-16 14:28:36 +0700

    add Player

--- core/src/logic/GameMap.java
+++ core/src/logic/GameMap.java
@@ -1,4 +1,9 @@
 package logic;
 
+import java.util.*;
+
 public class GameMap {
+    Player player;
+    final ArrayList<Monster> monsters = new ArrayList<>();
+    final ArrayList<Tile> tiles = new ArrayList<>();
 }
