5a37d94bc9257c7a251d576e2681a6ae83e8b817
Nut Idl <13thfloorguy@gmail.com>
2018-11-16 15:10:45 +0700

    add Player Constructor method

--- core/src/logic/Player.java
+++ core/src/logic/Player.java
@@ -1,4 +1,7 @@
 package logic;
 
 public class Player extends Creature {
+    public Player(String name, int maxHealth, int positionX, int positionY) {
+        super(name, maxHealth, positionX, positionY);
+    }
 }
