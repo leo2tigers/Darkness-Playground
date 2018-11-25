4460888d1829f4b814eff202aa8102bc9f016f5e
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2018-11-25 15:10:39 +0700

    Fixed image rendering

--- core/src/com/darknessplayground/game/DarknessPlayground.java
+++ core/src/com/darknessplayground/game/DarknessPlayground.java
@@ -2,18 +2,26 @@ package com.darknessplayground.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class DarknessPlayground extends ApplicationAdapter {
+	public static final int WIDTH = 1280;
+	public static final int HEIGHT = 720;
+	
 	SpriteBatch batch;
 	Texture img;
+	BitmapFont font;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		font = new BitmapFont();
+		font.setColor(Color.BLUE);
 	}
 
 	@Override
@@ -22,6 +30,7 @@ public class DarknessPlayground extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
+		font.draw(batch, "Hello World!", 200, 200);
 		batch.end();
 	}
 	
@@ -29,5 +38,6 @@ public class DarknessPlayground extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		font.dispose();
 	}
 }
