1edaa3cb8287e5cf87d41178c8e58c3b63eb6ba0
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2018-11-25 15:40:19 +0700

    Initialize

--- core/src/com/darknessplayground/game/screen/MainGame.java
+++ core/src/com/darknessplayground/game/screen/MainGame.java
@@ -1,11 +1,18 @@
 package com.darknessplayground.game.screen;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.darknessplayground.game.DarknessPlayground;
 
 public class MainGame implements Screen {
+	
+	private DarknessPlayground game;
 
-	public MainGame() {
+	public MainGame(DarknessPlayground game) {
 		// TODO Auto-generated constructor stub
+		this.game = game;
+		
 	}
 
 	@Override
@@ -17,6 +24,12 @@ public class MainGame implements Screen {
 	@Override
 	public void render(float delta) {
 		// TODO Auto-generated method stub
+		Gdx.gl.glClearColor(0.1f, 0.1f, 0.22f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		this.game.batch.begin();
+		
+		this.game.batch.end();
 
 	}
 
