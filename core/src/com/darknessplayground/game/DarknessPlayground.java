daa9e9d1c0fbcb1c8cb5a0730e82e8f4181dd7d5
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2018-11-26 20:16:36 +0700

    Revert main to screen-based

--- core/src/com/darknessplayground/game/DarknessPlayground.java
+++ core/src/com/darknessplayground/game/DarknessPlayground.java
@@ -1,53 +1,28 @@
 package com.darknessplayground.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import log.Log;
+import com.darknessplayground.game.screen.MainMenu;
 
 public class DarknessPlayground extends Game {
 	public static final int WIDTH = 1280;
 	public static final int HEIGHT = 720;
-
-    public SpriteBatch batch;
-    private BitmapFont font;
-	private String h = "Hello World!";
+	
+	public SpriteBatch batch;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		//img = new Texture("badlogic.jpg");
-		font = new BitmapFont();
-		font.setColor(Color.BLUE);
-        Log.begin();
+		this.setScreen(new MainMenu(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		if((Gdx.input.isKeyPressed(Input.Keys.ANY_KEY))) {
-			h = "fuck you!";
-		}
-
-		batch.begin();
-		//batch.draw(img, 0, 0);
-		font.draw(batch, h, 200, 200);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		//img.dispose();
-		font.dispose();
 	}
 }
