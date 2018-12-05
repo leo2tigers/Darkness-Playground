25037dd58d7184db7c60f211dfa8997d031518ab
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2018-12-05 21:23:21 +0700

    Typo fix

--- core/src/logic/creature/monster/MonsterType.java
+++ core/src/logic/creature/monster/MonsterType.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 public enum MonsterType {
     //TODO MonsterType(?)
     OwO ("OwO", 1, new Texture("Monsters/Normal OwO/new_owo.png")),
-	RANGED_OWO ("Ranged OwO", 2, new Texture("Monsters/Ranged OwO/new_ranged_owo.png"));
+	RANGED_OwO ("Ranged OwO", 2, new Texture("Monsters/Ranged OwO/new_ranged_owo.png"));
 
     public final String TypeName;
     public final int maxHealth;
